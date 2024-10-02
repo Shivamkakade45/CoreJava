0605d986733818ca3c3be701b193d161abb49062
Shivamkakade45 <shivamkakade95@gmail.com>
2024-10-02 21:53:48 +0530

    Update cgpa.java

--- cgpa.java
+++ cgpa.java
@@ -2,9 +2,9 @@
 // write a porogram to calculate a CGPA
 public class cgpa {
     public static void main(String[] args) {
-        int sub1 = 45;
-        int sub2 = 87;
-        int sub3 = 78;
+        float sub1 = 45;
+        float sub2 = 87;
+        float sub3 = 78;
         float cgpa = (sub1+sub2+sub3)/30;
         System.out.println(cgpa);
     }
