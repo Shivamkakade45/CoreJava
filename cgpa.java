94e8600792ac14f95155a29e9e8ec96e1765683b
Shivamkakade45 <shivamkakade95@gmail.com>
2024-10-02 19:21:31 +0530

    Add files via upload

---
+++ cgpa.java
@@ -0,0 +1,11 @@
+
+// write a porogram to calculate a CGPA
+public class cgpa {
+    public static void main(String[] args) {
+        int sub1 = 45;
+        int sub2 = 87;
+        int sub3 = 78;
+        float cgpa = (sub1+sub2+sub3)/30;
+        System.out.println(cgpa);
+    }
+}
