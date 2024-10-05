227e10e1aa1c580feeb90b2a616a0d0be273b575
Shivamkakade45 <shivamkakade95@gmail.com>
2024-10-05 15:17:11 +0530

    Add files via upload

---
+++ Arithmeticperator.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Arithmeticperator {
+    public static void main(String[] args) {
+      int p = 56;
+      int q = 98;
+      System.out.println(p*q);
+  System.out.println(p/q);
+  System.out.println(p%q);
+  System.out.println(p+q);
+  System.out.println(p*q);
+  
+  
+
+}
+
+}
