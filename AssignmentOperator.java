e74fcf2368ab5b38833521d020d2efd67dc55b74
Shivamkakade45 <shivamkakade95@gmail.com>
2024-10-05 16:29:57 +0530

    Add files via upload

---
+++ AssignmentOperator.java
@@ -0,0 +1,31 @@
+public class AssignmentOperator {
+
+    public static void main(String[] args) {
+        
+        int p = 10;
+        int q;
+
+        q=p;
+
+        System.out.println(q);
+
+        p += q; //p = p+q.
+
+        System.out.println(p);
+
+        p -= q; // p = p-q.
+
+        System.out.println(p);
+
+        p /= q; // p = p/q.
+
+        System.out.println(p);
+
+        p *= q; // p = p*q.
+
+        System.out.println(p);
+
+
+    }
+    
+}
