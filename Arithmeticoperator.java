8204284f8e8f7ad20e03df8dd575ac3156f44f33
Shivamkakade45 <shivamkakade95@gmail.com>
2024-10-05 15:18:01 +0530

    Update and rename Arithmeticperator.java to Arithmeticoperator.java

--- Arithmeticoperator.java
+++ Arithmeticoperator.java
@@ -1,10 +1,10 @@
 import java.util.*;
 
-public class Arithmeticperator {
+public class Arithmeticoperator {
     public static void main(String[] args) {
       int p = 56;
       int q = 98;
-      System.out.println(p*q);
+  System.out.println(p*q);
   System.out.println(p/q);
   System.out.println(p%q);
   System.out.println(p+q);
