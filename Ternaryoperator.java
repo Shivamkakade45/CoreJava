c4907419dd0202504a75bd164666e651ecc56a53
Shivamkakade45 <shivamkakade95@gmail.com>
2024-10-06 17:06:12 +0530

    Add files via upload

---
+++ Ternaryoperator.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class Ternaryoperator {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+
+        String result;
+
+        result = (a/2 == 0)? "even":"odd";
+
+        System.out.println(result);
+    }
+    
+}
